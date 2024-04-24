fbfd073c92243c8a197005e8fe6859be04b2b2f7
ADPKPI <adpkpicourse@gmail.com>
2024-04-24 06:32:47 +0300

    +

--- handlers.py
+++ handlers.py
@@ -3,6 +3,7 @@ from telegram import Bot
 import logging
 import os
 from config import chats
+import asyncio
 
 class FailureHandlingStrategy:
     def handle(self, server_name, check_name, error):
@@ -16,9 +17,10 @@ class StrategyFactory:
 class NotifyStrategy(FailureHandlingStrategy):
     def __init__(self):
         load_dotenv()
-        self.bot = Bot(token=os.getenv("MON_BOT_TOKEN"))
+        self.bot_token = os.getenv("MON_BOT_TOKEN")
+        self.bot = Bot(token=self.bot_token)
 
-    def handle(self, server_name, check_name):
+    async def handle(self, server_name, check_name):
         try:
             for id in chats:
                 await self.bot.send_message(chat_id=id, text=f"ТРЕВОГААААААААААААААААААААААААААААААААААААА {server_name} - {check_name} НАЕБНУЛСЯ!!!")

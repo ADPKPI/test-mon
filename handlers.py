7cdb7331ace247925692e680923b4a91678accae
ADPKPI <adpkpicourse@gmail.com>
2024-04-29 22:31:26 +0300

    +

--- handlers.py
+++ handlers.py
@@ -15,7 +15,7 @@ class StrategyFactory:
             return self.notify_failure_strategy
 
 class NotifyFailureStrategy(FailureHandlingStrategy):
-    def handle(self, server_name, check_name, oth):
+    def handle(self, server_name, check_name):
         url = 'http://127.0.0.1:5001/alert'
         for id in chats:
             data = {

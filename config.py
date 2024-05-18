6fa90d4216b2ff0ea6d33f3550189337b2155987
Artem Prokhorenko <adpkpicourse@gmail.com>
2024-05-18 16:20:07 +0300

    +

--- config.py
+++ config.py
@@ -31,6 +31,36 @@ servers = [
         ]
     },
     {
+        "name": "API1",
+        "host": "206.54.170.126",
+        "user": "root",
+        "password": "O15v26LKb6mxjFX0eX",
+        "checks": [
+            {"type": "ping", "name": "PING"},
+            {"type": "telnet", "name": "SSH", "port": 22},
+            {"type": "telnet", "name": "API port", "port": 5000},
+            {"type": "service", "name": "API service", "service": "adp-db-api.service"},
+            {"type": "cpu", "name": "CPU"},
+            {"type": "ram", "name": "RAM"},
+            {"type": "disk_space", "name": "DISK SPACE"}
+        ]
+    },
+    {
+        "name": "API2",
+        "host": "206.54.191.62",
+        "user": "root",
+        "password": "wQ5enUo7SP1d8rF9X5",
+        "checks": [
+            {"type": "ping", "name": "PING"},
+            {"type": "telnet", "name": "SSH", "port": 22},
+            {"type": "telnet", "name": "API port", "port": 5000},
+            {"type": "service", "name": "API service", "service": "adp-db-api.service"},
+            {"type": "cpu", "name": "CPU"},
+            {"type": "ram", "name": "RAM"},
+            {"type": "disk_space", "name": "DISK SPACE"}
+        ]
+    },
+    {
         "name": "TG1",
         "host": "78.140.189.246",
         "user": "root",
@@ -40,6 +70,21 @@ servers = [
             {"type": "telnet", "name": "SSH", "port": 22},
             {"type": "service", "name": "Client Bot", "service":"adp-client-bot.service"},
             {"type": "service", "name": "Shop Bot", "service":"adp-shop-bot.service"},
+            {"type": "cpu", "name": "CPU"},
+            {"type": "ram", "name": "RAM"},
+            {"type": "disk_space", "name": "DISK SPACE"}
+        ]
+    },
+    {
+        "name": "TG2",
+        "host": "199.80.52.35",
+        "user": "root",
+        "password": "yPYyE83se79L5p3iMF",
+        "checks": [
+            {"type": "ping", "name": "PING"},
+            {"type": "telnet", "name": "SSH", "port": 22},
+            {"type": "service", "name": "Client Bot", "service": "adp-client-bot.service"},
+            {"type": "service", "name": "Shop Bot", "service": "adp-shop-bot.service"},
             {"type": "cpu", "name": "CPU"},
             {"type": "ram", "name": "RAM"},
             {"type": "disk_space", "name": "DISK SPACE"}
